7a1b5d3da14ffe0ea53c5aa895980345591e1341
oahmaro <osamaahmaro@gmail.com>
2018-08-09 19:35:07 +0300

    feat: add middleware

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import DeckList from './components/DeckList'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import middleware from './middleware'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={createStore(reducer, middleware)}>
         <View style={styles.container}>
           <DeckList />
         </View>
