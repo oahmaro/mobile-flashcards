982c66efdb0c7bbeae8a25303791915ec6fa9e74
oahmaro <osamaahmaro@gmail.com>
2018-08-09 21:08:33 +0300

    refactor: add tab navigator to app

--- App.js
+++ App.js
@@ -5,12 +5,44 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+import { createMaterialTopTabNavigator } from 'react-navigation'
+import { white, blue } from './utils/colors'
+
+const Tabs = createMaterialTopTabNavigator({
+  Deck: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'DECKS',
+    }
+  },
+  NewDeck: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'NEW DECK '
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: white,
+    style: {
+      height: 56,
+      backgroundColor: blue, 
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer, middleware)}>
-          <DeckList />
+          <Tabs />
       </Provider>
     );
   }
---
+++ utils/colors.js
@@ -0,0 +1,2 @@
+export const blue = '#546a8e'
+export const white = '#fff'
