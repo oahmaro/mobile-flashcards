918c7d9e4d5c36d86cfa5f81afc15e56c82717de
oahmaro <osamaahmaro@gmail.com>
2018-08-09 19:29:18 +0300

    feat: add redux to app

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DeckList from './components/DeckList'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <DeckList />
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <DeckList />
+        </View>
+      </Provider>
     );
   }
 }
