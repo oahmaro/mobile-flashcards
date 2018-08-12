c39b75558922c0348c0846534a1d3022843dd590
oahmaro <osamaahmaro@gmail.com>
2018-08-12 09:16:21 +0300

    feat: create AddCard Component

---
+++ components/AddCard.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { TextInput, View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+
+class AddCard extends Component {
+    state = {
+        input: '',
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput placeholder='Question'/>
+                <TextInput placeholder='Answer'/>
+                <TouchableOpacity onPress={() => console.log(pressed)}>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default AddCard
+
+const styles = StyleSheet.create({
+
+})
